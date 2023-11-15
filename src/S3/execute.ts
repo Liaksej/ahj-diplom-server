a6c709792e7719113618719de55c8864b2cd5e97
Liaksej <tarusov.liaksej@gmail.com>
2023-11-15 20:06:23 +0100

    Implement multipart upload functionality in S3 service
    Refactored the S3 service to use multipart uploads for large files instead of a single PutObjectCommand. Updated the code to divide large files into smaller parts, upload them separately using the UploadPartCommand, and then complete the upload with CompleteMultipartUploadCommand. Removed unnecessary console logs and added error handling for upload failures.

---
+++ src/S3/execute.ts
@@ -0,0 +1,6 @@
+import { main } from "./index";
+
+(async () => {
+  const result = await main("IMG_7585_(2).mov");
+  console.log(result);
+})();
--- src/S3/index.ts
+++ src/S3/index.ts
@@ -1,58 +1,109 @@
 import {
-  GetObjectCommand,
-  ListObjectsCommand,
-  PutObjectCommand,
+  CreateMultipartUploadCommand,
+  UploadPartCommand,
+  CompleteMultipartUploadCommand,
+  AbortMultipartUploadCommand,
   S3Client,
+  PutObjectAclCommand,
 } from "@aws-sdk/client-s3";
 import { defaultProvider } from "@aws-sdk/credential-provider-node";
 import fs from "fs";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-class S3 {
-  s3: S3Client;
-  constructor(config) {
-    this.s3 = new S3Client(config);
-  }
+const twentyFiveMB = 25 * 1024 * 1024;
 
-  async uploadFile(bucketName: string, fileName: string, filePath: string) {
-    const params = new PutObjectCommand({
+export const createString = (size = twentyFiveMB) => {
+  return "x".repeat(size);
+};
+
+export const main = async (objectKey: string) => {
+  const s3Client = new S3Client({
+    region: "us-east-2",
+    credentials: defaultProvider({
+      profile: "default",
+    }),
+  });
+  const bucketName = "liaksejs-bucket";
+  const key = objectKey;
+  const filePath = "./IMG_7585_(2).mov";
+  const buffer = fs.readFileSync(filePath);
+
+  let uploadId;
+
+  try {
+    const multipartUpload = await s3Client.send(
+      new CreateMultipartUploadCommand({
+        Bucket: bucketName,
+        Key: key,
+      }),
+    );
+
+    uploadId = multipartUpload.UploadId;
+
+    const uploadPromises = [];
+    // Multipart uploads require a minimum size of 5 MB per part.
+    const partSize = Math.ceil(buffer.length / 5);
+
+    // Upload each part.
+    for (let i = 0; i < 5; i++) {
+      const start = i * partSize;
+      const end = start + partSize;
+      uploadPromises.push(
+        s3Client
+          .send(
+            new UploadPartCommand({
+              Bucket: bucketName,
+              Key: key,
+              UploadId: uploadId,
+              Body: buffer.subarray(start, end),
+              PartNumber: i + 1,
+            }),
+          )
+          .then((d) => {
+            console.log("Part", i + 1, "uploaded");
+            return d;
+          }),
+      );
+    }
+
+    const uploadResults = await Promise.all(uploadPromises);
+
+    await s3Client.send(
+      new CompleteMultipartUploadCommand({
+        Bucket: bucketName,
+        Key: key,
+        UploadId: uploadId,
+        MultipartUpload: {
+          Parts: uploadResults.map(({ ETag }, i) => ({
+            ETag,
+            PartNumber: i + 1,
+          })),
+        },
+      }),
+    );
+
+    const putObjectAclCommand = new PutObjectAclCommand({
       Bucket: bucketName,
-      Key: fileName,
-      Body: fs.createReadStream(filePath),
+      Key: objectKey,
       ACL: "public-read",
     });
 
-    try {
-      await this.s3.send(params);
-      console.log("File upload successful");
-      return this.getPreSignedUrl(bucketName, fileName);
-    } catch (err) {
-      console.error("File upload failed:", err);
+    await s3Client.send(putObjectAclCommand);
+
+    return `https://${bucketName}.s3.us-east-2.amazonaws.com/${key}`;
+
+    // Verify the output by downloading the file from the Amazon Simple Storage Service (Amazon S3) console.
+    // Because the output is a 25 MB string, text editors might struggle to open the file.
+  } catch (err) {
+    console.error(err);
+
+    if (uploadId) {
+      const abortCommand = new AbortMultipartUploadCommand({
+        Bucket: bucketName,
+        Key: key,
+        UploadId: uploadId,
+      });
+
+      await s3Client.send(abortCommand);
     }
   }
-
-  async generateFileUrl(bucketName: string, fileName: string) {
-    const command= new GetObjectCommand({ Bucket: bucketName, Key: key });
-
-    return getSignedUrl(this.s3, command, { expiresIn: 3600 }); // URL expires in 1 hour
-}
-
-const s3 = new S3Client({
-  region: "us-east-2",
-  credentials: defaultProvider({
-    profile: "default",
-  }),
-});
-
-const run = async () => {
-  try {
-    const data = await s3.send(
-      new ListObjectsCommand({ Bucket: "liaksejs-bucket" }),
-    );
-    console.log("Success", data);
-  } catch (err) {
-    console.log("Error", err);
-  }
 };
-
-run();
